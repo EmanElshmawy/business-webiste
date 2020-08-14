522f3eec80a38d30157005e651b879cbda255133
Eman <emanmohamed017@gmail.com>
2020-08-15 01:08:01 +0200

    styling updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,9 @@
-jQuery(document).ready(function ($) {
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        nav: true,
-        navText: ["<div class='icon icon-arrow-left7'><i class='fas fa-angle-left'></i></div>", "<div class='icon icon-arrow-right7' ><i class='fas fa-angle-right'></div>"], items: 1,
-        dots:false,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:1
-            },
-            1000:{
-                items:1
-            }
-        }
-    })
-  // navbar
-  var scrollTop;
-  if ($('#navbar').offset().top >= 100) {
-    $('#navbar').addClass('scrolled-nav');
-  } else {
-    $('#navbar').removeClass('scrolled-nav');
-  }
-  $(window).scroll(function () {
-    scrollTop = $(window).scrollTop();
-    if (scrollTop >= 100) {
-      $('#navbar').addClass('scrolled-nav');
-    } else if (scrollTop < 100) {
-      $('#navbar').removeClass('scrolled-nav');
-    }
-  });
-  $.holdReady(false);
-  $(".navbar .nav-link").on('click', function (event) {
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      // event.preventDefault();
-      //mPS2id-highlight
-      // Store hash
-      var hash = this.hash;
-      console.log($(hash).offset().top)
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top - 0
-
-      }, 800, function () {
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-
-  $('.r-section .nav-tabs .nav-item:hover .nav-link').click(function (event) {
-
-    event.preventDefault();
-
-  });
-
-  });
-  // preloader
-  (function ($) {
+// preloader
+(function ($) {
     $.fn.preloadinator = function (options) {
         'use strict';
-  
+
         var settings = $.extend({
                 scroll: false,
                 minTime: 0,
@@ -79,22 +15,22 @@ jQuery(document).ready(function ($) {
             }, options),
             preloader = this,
             start = new Date().getTime();
-  
+
         $.fn.preloadinator.disableScroll = function () {
             $('body').css('overflow', 'hidden');
-  
+
             if (typeof settings.afterDisableScroll == 'function') {
                 settings.afterDisableScroll.call(this);
             }
         }
-  
+
         $.fn.preloadinator.enableScroll = function () {
-  
+
             if (typeof settings.afterEnableScroll == 'function') {
                 settings.afterEnableScroll.call(this);
             }
         }
-  
+
         $.fn.preloadinator.removePreloader = function () {
             $('body').css('overflow', 'auto');
 
@@ -107,49 +43,117 @@ jQuery(document).ready(function ($) {
                 }
             });
         }
-  
+
         $.fn.preloadinator.minTimeElapsed = function () {
             var now = new Date().getTime(),
                 elapsed = now - start;
-  
+
             if (elapsed >= settings.minTime) {
                 return true;
             } else {
                 return false;
             }
         }
-  
+
         if (settings.scroll === false) {
             $.fn.preloadinator.disableScroll();
         }
-  
+
         $(window).on('load', function () {
             if ($.fn.preloadinator.minTimeElapsed()) {
                 $.fn.preloadinator.removePreloader();
             } else {
                 var now = new Date().getTime(),
                     elapsed = now - start;
-  
+
                 setTimeout($.fn.preloadinator.removePreloader, settings.minTime - elapsed);
             }
         });
-  
+
         return this;
     }
-  }(jQuery));
-  $('.js-preloader').preloadinator({
-    minTime:1000
-  });
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-36251023-1']);
-  _gaq.push(['_setDomainName', 'jqueryscript.net']);
-  _gaq.push(['_trackPageview']);
-  
-  (function () {
+}(jQuery));
+$('.js-preloader').preloadinator({
+    minTime: 1000
+});
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-36251023-1']);
+_gaq.push(['_setDomainName', 'jqueryscript.net']);
+_gaq.push(['_trackPageview']);
+
+(function () {
     var ga = document.createElement('script');
     ga.type = 'text/javascript';
     ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(ga, s);
-  })();
+})();
+
+
+
+jQuery(document).ready(function ($) {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        nav: true,
+        navText: ["<div class='icon icon-arrow-left7'><img src='./assets/images/back.svg'></div>", "<div class='icon icon-arrow-right7' ><img src='./assets/images/next.svg'></div>"],
+        items: 1,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
+    // navbar
+    var scrollTop;
+    if ($('#navbar').offset().top >= 100) {
+        $('#navbar').addClass('scrolled-nav');
+    } else {
+        $('#navbar').removeClass('scrolled-nav');
+    }
+    $(window).scroll(function () {
+        scrollTop = $(window).scrollTop();
+        if (scrollTop >= 100) {
+            $('#navbar').addClass('scrolled-nav');
+        } else if (scrollTop < 100) {
+            $('#navbar').removeClass('scrolled-nav');
+        }
+    });
+    $.holdReady(false);
+    $(".navbar .nav-link").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            // event.preventDefault();
+            //mPS2id-highlight
+            // Store hash
+            var hash = this.hash;
+            console.log($(hash).offset().top)
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - 0
+
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+    $(".loader-modal").animate({
+        opacity: 1,
+        left: 0
+    }, 8000, "linear", function () {
+        $('#myModal').modal('show'); 
+
+    });
+});
